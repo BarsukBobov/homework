08edd11f8f2e24c3b966f94e51c939104e6f5013
MII <igoryha-701@mail.ru>
2023-09-20 20:45:22 +0500

    added the solution of the second point of the first task

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -17,5 +17,17 @@ def get_prime_numbers(n: int) -> List[int]:
     return lst
 
 
-res = get_prime_numbers(0)
+def get_prime_numbers_fixed_count(n: int) -> List[int]:
+    lst = []
+    i = 2
+    while len(lst) < n:
+        lst2 = [i for j in lst if i % j == 0]
+        lst += [] if lst2 else [i]
+        i += 1
+    return lst
+
+
+res = get_prime_numbers(5)
 print(res)
+res2 = get_prime_numbers_fixed_count(5)
+print(res2)
